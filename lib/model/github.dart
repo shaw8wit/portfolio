b04caee8321d2077acd8c7ed675767ef8fad3223
shaw8wit <shaw8wit@gmail.com>
2020-12-31 20:42:11 +0530

    projects structure edited

--- lib/model/github.dart
+++ lib/model/github.dart
@@ -4,7 +4,15 @@ import 'package:flutter/material.dart';
 
 class GithubClass {
   final String name, description, url;
-  GithubClass({this.name, this.description, this.url});
+  final int stars, forks, watchers;
+  GithubClass({
+    this.name,
+    this.description,
+    this.url,
+    this.stars,
+    this.forks,
+    this.watchers,
+  });
 }
 
 class Github with ChangeNotifier {
@@ -15,11 +23,19 @@ class Github with ChangeNotifier {
     try {
       final response = await http.get(_url);
       final data = json.decode(response.body);
-      data.forEach((e) => l.add(GithubClass(
-            name: e['name'],
-            description: e['description'],
-            url: e['html_url'],
-          )));
+      data.forEach((e) => {
+            if (e['fork'] == false)
+              {
+                l.add(GithubClass(
+                  name: e['name'],
+                  description: e['description'],
+                  url: e['html_url'],
+                  stars: e['stargazers_count'],
+                  forks: e['watchers_count'],
+                  watchers: e['forks_count'],
+                ))
+              }
+          });
       notifyListeners();
     } catch (e) {
       print(e);
